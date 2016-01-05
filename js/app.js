fbd94c64cf2df9ce47113295dcd9cd2361209aad
Henry Rogers <hrogers@sugarcrm.com>
2016-01-04 21:43:33 -0500

    SUCROSE-3: add gauge, line area and tree chart types; also added toggle options

--- js/app.js
+++ js/app.js
@@ -73,7 +73,11 @@ $(function () {
             self.resetChartSize();
             self.loadData(self.data.file.val);
           });
-
+          $('button[data-action=toggle]').on('click', function (e) {
+            $('.sc-form-container').toggleClass('hidden');
+            $('.sc-chart-container').toggleClass('full-width');
+            self.chartResizer(self.Chart)(e);
+          });
         },
         resetChartSize: function () {
           $chart.removeAttr('style');
@@ -128,6 +132,9 @@ $(function () {
             case 'textarea':
               control = this.textareaControl(v, n);
               break;
+            case 'button':
+              control = this.buttonControl(v, n);
+              break;
             default:
               control = this.textControl(v, n);
               break;
@@ -152,19 +159,26 @@ $(function () {
           var select = '<select name="' + n + '">';
           v.each(function (r) {
             select += '<option value="' + r.value + '">' + r.label + '</option>';
-          })
+          });
           select += '</select>';
           return select;
         },
         textareaControl: function (v, n) {
-          var value = v.map(function (o) { return o.value }).join(' '),
+          var value = v.map(function (o) { return o.value; }).join(' '),
               textarea = '<textarea name="' + n + '">' + value + '</textarea>';
           return textarea;
         },
         textControl: function (v, n) {
-          var value = v.map(function (o) { return o.value }).join(' '),
+          var value = v.map(function (o) { return o.value; }).join(' '),
               text = '<input type="text" name="' + n + '" value="' + value + '">';
           return text;
+        },
+        buttonControl: function (v, n) {
+          var button = '';
+          v.each(function (b) {
+              button += '<button type="button" name="' + n + '" data-control="' + b.value + '">' + b.label + '</button>';
+          });
+          return button;
         },
         // Render chart without data update
         chartRenderer: function () {
@@ -234,7 +248,7 @@ $(function () {
                   }
 
                   // TODO: redo this data translation mess
-                  if (this.type === 'treemap') {
+                  if (this.type === 'treemap' || this.type === 'tree') {
                     chartData = json;
                     this.colorLength = 0;
                   } else {
@@ -398,187 +412,6 @@ $(function () {
         }
       };
 
-  function sucroseCharts(type) {
-    var chart,
-        showTitle = true,
-        showLegend = true,
-        tooltips = true;
-
-    switch (type) {
-      case 'pie':
-        chart = sucrose.models.pieChart()
-          .donut(true)
-          .donutRatio(0.5)
-          .pieLabelsOutside(true)
-          .maxRadius(250)
-          .minRadius(100)
-          .tooltipContent(function (key, x, y, e, graph) {
-            return '<p>Stage: <b>' + key + '</b></p>' +
-                   '<p>Amount: <b>$' + parseInt(y) + 'K</b></p>' +
-                   '<p>Percent: <b>' + x + '%</b></p>';
-          });
-          // .rotateDegrees(rotate)
-          // .arcDegrees(arc)
-          // .fixedRadius(function (container, chart) {
-          //   var n = d3.select('#chart1').node(),
-          //       r = Math.min(n.clientWidth * 0.25, n.clientHeight * 0.4);
-          //   return Math.max(r, 75);
-          // });
-
-        chart.pie
-          .textureFill(false);
-        break;
-      case 'funnel':
-        chart = sucrose.models.funnelChart()
-          .tooltipContent(function (key, x, y, e, graph) {
-            return '<p>Stage: <b>' + key + '</b></p>' +
-                   '<p>Amount: <b>$' + parseInt(y) + 'K</b></p>' +
-                   '<p>Percent: <b>' + x + '%</b></p>';
-          });
-
-        chart.funnel
-          .textureFill(true);
-        break;
-      case 'multibar':
-        chart = sucrose.models.multiBarChart()
-          .showControls(true)
-          .stacked(true)
-          // .margin({top: 0, right: 0, bottom: 0, left: 0})
-          // TODO: replace when PAT-2448 is merged
-          // .valueFormat(function (d) {
-          //   var si = d3.formatPrefix(d, 2);
-          //   return d3.round(si.scale(d), 2) + si.symbol;
-          // })
-          .tooltipContent(function (key, x, y, e, graph) {
-            return '<p>Outcome: <b>' + key + '</b></p>' +
-                   '<p>Percentage: <b>' + x + '%</b></p>' +
-                   '<p>Amount: <b>$' + parseInt(y, 10) + 'K</b></p>';
-          })
-          .seriesClick(function (data, eo, chart) {
-            chart.dataSeriesActivate(eo);
-          })
-          .overflowHandler(function (d) {
-            var b = $('body');
-            b.scrollTop(b.scrollTop() + d);
-          });
-
-        chart.multibar
-          .textureFill(true);
-
-        chart.yAxis
-          .tickFormat(chart.multibar.valueFormat());
-        break;
-      case 'line':
-        chart = sucrose.models.lineChart()
-          .showControls(true)
-          .useVoronoi(true)
-          .clipEdge(false)
-          .tooltipContent(function (key, x, y, e, graph) {
-            var content = '<p>Category: <b>' + key + '</b></p>' +
-                   '<p>Amount: <b>$' + parseInt(y) + 'M</b></p>',
-                dateCheck = new Date(x);
-            if (dateCheck instanceof Date && !isNaN(dateCheck.valueOf())) {
-              content += '<p>Date: <b>' + x + '</b></p>';
-            }
-            return content;
-          });
-        break;
-      case 'bubble':
-        chart = sucrose.models.bubbleChart()
-          .x(function (d) { return d3.time.format('%Y-%m-%d').parse(d.x); })
-          .y(function (d) { return d.y; })
-          .tooltipContent(function (key, x, y, e, graph) {
-            return '<p>Assigned: <b>' + e.point.assigned_user_name + '</b></p>' +
-                   '<p>Amount: <b>$' + d3.format(',.2d')(e.point.opportunity) + '</b></p>' +
-                   '<p>Close Date: <b>' + d3.time.format('%x')(d3.time.format('%Y-%m-%d').parse(e.point.x)) + '</b></p>' +
-                   '<p>Probability: <b>' + e.point.probability + '%</b></p>' +
-                   '<p>Account: <b>' + e.point.account_name + '</b></p>';
-          })
-          .groupBy(function (d) {
-              return d.assigned_user_name;
-          })
-          .filterBy(function (d) { return d.probability; });
-        break;
-      case 'treemap':
-        chart = sucrose.models.treemapChart()
-          .leafClick(function (d) {
-            alert('leaf clicked');
-          })
-          .getSize(function (d) { return d.size; });
-        showTitle = false;
-        showLegend = false;
-        break;
-      case 'pareto':
-        chart = sucrose.models.paretoChart()
-          .stacked(true)
-          .clipEdge(false)
-          .yAxisTickFormat(function(d) {
-            var si = d3.formatPrefix(d, 2);
-            return '$' + d3.round(si.scale(d), 2) + si.symbol;
-          })
-          .quotaTickFormat(function(d) {
-            var si = d3.formatPrefix(d, 2);
-            return '$' + d3.round(si.scale(d), 2) + si.symbol;
-          })
-          .tooltipBar(function(key, x, y, e, graph) {
-            return '<p><b>' + key + '</b></p>' +
-              '<p><b>' + y + '</b></p>' +
-              '<p><b>' + x + '%</b></p>';
-          })
-          .tooltipLine(function(key, x, y, e, graph) {
-            return '<p><p>' + key + ': <b>' + y + '</b></p>';
-          })
-          .tooltipQuota(function(key, x, y, e, graph) {
-            return '<p>' + e.key + ': <b>$' + y + '</b></p>';
-          })
-          .barClick(function(data, eo, chart, container) {
-              var d = eo.series,
-                  selectedSeries = eo.seriesIndex;
-
-              chart.dispatch.tooltipHide();
-
-              d.disabled = !d.disabled;
-
-              if (!chart.stacked()) {
-                  data.filter(function(d) {
-                      return d.series === selectedSeries && d.type === 'line';
-                  }).map(function(d) {
-                      d.disabled = !d.disabled;
-                      return d;
-                  });
-              }
-
-              // if there are no enabled data series, enable them all
-              if (!data.filter(function(d) {
-                  return !d.disabled && d.type === 'bar';
-              }).length) {
-                  data.map(function(d) {
-                      d.disabled = false;
-                      container.selectAll('.sc-series').classed('disabled', false);
-                      return d;
-                  });
-              }
-
-              container.call(chart);
-          });
-        break;
-    }
-
-    chart
-      .showTitle(showTitle)
-      .showLegend(showLegend)
-      .tooltips(tooltips);
-
-    if (chart.seriesClick) {
-      chart
-        .seriesClick(function (data, eo, chart) {
-          chart.dataSeriesActivate(eo);
-        });
-    }
-
-    return chart;
-  }
-
   function loadChart(type) {
     if (!type) {
       return;
@@ -624,6 +457,12 @@ $(function () {
       // Combine common and custom Manifest UIs
       Object.merge(Manifest, chartManifest, true);
 
+      Object.each(Manifest.ui, function (k, v, d) {
+        if (v.hidden) {
+          delete d[k];
+        }
+      });
+
       // Data containers persisted in localStorage
       store.set('examples-type', type);
       // Set Application scope variables
@@ -653,8 +492,6 @@ $(function () {
         .removeClass('active')
         .filter('[data-type=' + type + ']')
         .addClass('active');
-
-      console.log('type: ', type);
     });
   }
 
---
+++ manifest/area_demo.js
@@ -0,0 +1,104 @@
+
+  var chartManifest = {
+    id: 'sucrose-area',
+    type: 'area',
+    title: 'Stacked Area Chart',
+    // Use these option presets to set the form input values
+    // These chart options will be set
+    optionPresets: {
+      file: 'line_data',
+      show_labels: '1'
+    },
+    yAxisLabel: 'Y-axis label',
+    xAxisLabel: 'X-axis label',
+    // These options should match the names of all form input names
+    // Set them to the default value as expected by sucrose
+    // If the option remains the default value, the chart option will not be set
+    optionDefaults: {
+      file: '',
+      color: 'default',
+      direction: 'ltr',
+      show_labels: '0',
+      mirror_axis: 'lab',
+      tick_display: ['wrap', 'stagger', 'rotate']
+    },
+    ui: {
+      '[name=file]': {
+        // Set data file options in Manifest control
+        values: [
+          {value: 'line_data', label: 'Test Line Chart Data #1'}
+        ]
+      },
+      '[name=mirror_axis]': {
+        init: function ($o) {
+          this.initControl($o.attr('name'));
+        },
+        bind: function (d, v, $o) {
+          return this.bindControl(d, $o.attr('name'), v, this.chartUpdater());
+        },
+        chartInit: function (v, self) {
+          self.Chart.yAxis
+            .orient(v === 'lab' ? 'left' : 'right');
+          self.Chart.xAxis
+            .orient(v === 'lab' ? 'bottom' : 'top');
+        },
+        check: /0|1/i,
+        events: 'change.my',
+        title: 'Mirror Axes',
+        type: 'radio',
+        values: [
+          {value: 'lab', label: 'Left/Bottom'},
+          {value: 'rat', label: 'Right/Top'}
+        ]
+      },
+      '[name=show_labels]': {
+        init: function ($o) {
+          this.initControl($o.attr('name'));
+        },
+        bind: function (d, v, $o) {
+          return this.bindControl(d, $o.attr('name'), v, this.chartRenderer());
+        },
+        chartInit: function (v, self) {
+          var value = !!parseInt(v, 10);
+          self.Chart.xAxis.axisLabel(value ? self.xAxisLabel : null);
+          self.Chart.yAxis.axisLabel(value ? self.yAxisLabel : null);
+        },
+        check: /0|1/i,
+        events: 'change.my',
+        title: 'Show Axes Labels',
+        type: 'radio',
+        values: [
+          {value: '0', label: 'No'},
+          {value: '1', label: 'Yes'}
+        ]
+      },
+      '[name=tick_display]': {
+        init: function ($o) {
+          this.initControl($o.attr('name'));
+        },
+        bind: function (d, v, $o) {
+          return this.bindControl(d, $o.attr('name'), v, this.chartRenderer());
+        },
+        chartInit: function (v, self) {
+          var wrapTicks = $.inArray('wrap', v) !== -1,
+              staggerTicks = $.inArray('stagger', v) !== -1,
+              rotateTicks = $.inArray('rotate', v) !== -1;
+          self.Chart
+            .wrapTicks(wrapTicks)
+            .staggerTicks(staggerTicks)
+            .rotateTicks(rotateTicks);
+        },
+        check: /wrap|stagger|rotate/i,
+        events: 'change.my',
+        title: 'Tick Display Methods',
+        type: 'checkbox',
+        values: [
+          {value: 'wrap', label: 'Wrap'},
+          {value: 'stagger', label: 'Stagger'},
+          {value: 'rotate', label: 'Rotate'}
+        ]
+      }
+    }
+  };
+  var cachedManifest = $.my.tojson(chartManifest);
+  console.log(cachedManifest);
