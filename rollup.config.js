da6bbe02652551460e27d74b9274c80c20b6ccae
Henry Rogers <hrogers@sugarcrm.com>
2017-04-24 03:31:16 -0400

    SUCROSE-3:RPT-156: update build scripts to bundle custom d3v4 and support sucrose build targets

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
@@ -6,9 +7,19 @@ export default {
   dest: './build/sucrose.js',
   format: 'umd',
   // sourceMap: 'inline',
-  external: ['d3', 'd3fc-rebind'],
+  external: [
+    'd3v4',
+    'd3fc-rebind',
+  ],
   // treeshake: false,
   plugins: [
+    replace({
+      exclude: 'node_modules/**',
+      values: {
+        ENV_DEV: (process.env.DEV || true),
+        ENV_BUILD: (process.env.BUILD || 'sc'),
+      },
+    }),
     resolve({
       jsnext: true,
       main: true,
@@ -16,7 +27,7 @@ export default {
     }),
   ],
   globals: {
+    'd3v4': 'd3v4',
     'd3fc-rebind': 'fc',
-    'd3': 'd3',
   },
 };
