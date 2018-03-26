f5384123e545b320abbc48dda348bb5de4baadef
Henry Rogers <hrogers@sugarcrm.com>
2018-03-26 15:34:26 -0400

    Add download.js package and fix generateImage methods for Safari

--- examples/js/app.js
+++ examples/js/app.js
@@ -808,13 +808,6 @@ function generatePackage(e) {
     });
 }
 
-//https://developer.mozilla.org/en-US/docs/Web/API/Window.btoa
-//http://techslides.com/save-svg-as-an-image/
-//http://tutorials.jenkov.com/svg/svg-and-css.html
-//https://developer.mozilla.org/en-US/docs/Web/HTML/Canvas/Drawing_DOM_objects_into_a_canvas
-//http://spin.atomicobject.com/2014/01/21/convert-svg-to-png/
-//https://github.com/exupero/saveSvgAsPng
-
 function generateImage(e) {
   e.preventDefault();
   e.stopPropagation();
@@ -856,27 +849,29 @@ function generateImage(e) {
       var width = chart.width();
       var height = chart.height();
       var dom = chart.find('svg').html();
-      var svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="' +
-                 width + '" height="' + height + '" viewBox="0 0 ' + width + ' ' + height + '" class="sucrose sc-chart sc-print">' +
-                '<defs><style rel="stylesheet/less" type="text/css"><![CDATA[' + css + ']]></style></defs>' + dom + '</svg>';
+      var svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" ' +
+                'width="' + width + '" height="' + height + '" viewBox="0 0 ' + width + ' ' + height + '" ' +
+                'class="sucrose sc-chart sc-print" style="width:auto;height:auto">' +
+                '<defs><style rel="stylesheet" type="text/css"><![CDATA[' + css.replace('url("../', 'url("') + ']]></style></defs>' +
+                dom + '</svg>';
+
       var url = 'data:image/svg+xml;charset=utf-8;base64,' + window.btoa(reEncode(svg));
       var canvas = document.createElement('canvas');
       var ctx = canvas.getContext('2d');
       var img = new Image();
 
-      document.body.appendChild(canvas);
-
       canvas.width = width;
       canvas.height = height;
       canvas.className = 'sc-image-canvas';
+      document.body.appendChild(canvas);
+
       ctx.fillStyle = '#ffffff';
       ctx.fillRect(0, 0, canvas.width, canvas.height);
-
       img.onload = function () {
           var uri;
-          ctx.drawImage(img, 0, 0);
+          ctx.drawImage(img, 0, 0, width, height);
           uri = canvas.toDataURL('image/png');
-          openTab(uri);
+          download(uri, 'download.png', 'image/png');
           ctx.clearRect(0, 0, width, height);
           canvas.remove();
       };
@@ -884,6 +879,15 @@ function generateImage(e) {
       img.src = url;
     });
 }
+
+// RESEARCH
+//https://developer.mozilla.org/en-US/docs/Web/API/Window.btoa
+//http://techslides.com/save-svg-as-an-image/
+//http://tutorials.jenkov.com/svg/svg-and-css.html
+//https://developer.mozilla.org/en-US/docs/Web/HTML/Canvas/Drawing_DOM_objects_into_a_canvas
+//http://spin.atomicobject.com/2014/01/21/convert-svg-to-png/
+//https://github.com/exupero/saveSvgAsPng
+//http://danml.com/download.html
 
 function generateData(e) {
   e.preventDefault();
