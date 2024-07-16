f0e33e60f6c403d216b232c813e2b461bc30287b
a.krivickiy <andrei.krivickiy@sugarcrm.com>
2024-07-16 18:27:25 +0300

    SS-4191: Address Sucrose Issues
    Remove the examples folder from the Sucrose repo and all references to it in Sucrose.

--- build/package.js
+++ build/package.js
@@ -1,5 +1,5 @@
 export var name = "sucrose";
-export var version = "0.8.3";
+export var version = "0.8.4";
 export var description = "Interactive Charts for Business Applications";
 export var keywords = ["charts","d3","visualization","svg","mobile","canvas"];
 export var homepage = "http://sucrose.io/";
